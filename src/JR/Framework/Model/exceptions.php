ff3263412f6bd5a9d7faefbadc748ef0598d0a6b
RebendaJiri <jr.rebenda@rje.cz>
2015-06-26 20:16:41 +0200

    added basic model exceptions

---
+++ src/JR/Framework/Model/exceptions.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace JR\Framework\Model;
+
+/**
+ * @author RebendaJiri <jiri.rebenda@htmldriven.com>
+ */
+class ModelException extends \Exception
+{
+	
+}
+
+/**
+ * @author RebendaJiri <jiri.rebenda@htmldriven.com>
+ */
+class EntityNotFoundException extends ModelException
+{
+	
+}
